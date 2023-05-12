fda1edccdc7956fae47b91345ed9eb0d2c553927
Oscar Gallego <hola@oscargallegoruiz.com>
2023-05-12 13:05:38 +0200

    add StrictMode

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
