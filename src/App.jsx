f1c3a5e770d76789a696dab2467c0eaa1018d1c5
Oscar <info@oscargallegoruiz.com>
2023-11-10 17:47:49 +0100

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from "react";
 import "./App.module.scss";
 import BaseLayout from "./components/BaseLayout";
 import { BrowserRouter } from "react-router-dom";
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <BaseLayout />
-      </BrowserRouter>
-    </div>
-  );
+   return (
+      <div>
+         <BrowserRouter>
+            <Analytics />
+            <BaseLayout />
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
